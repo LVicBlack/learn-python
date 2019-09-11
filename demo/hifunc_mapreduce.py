6bb83cc66bc367904bfd0993de2a06ed9ed1a858
vic-company <1173646430@qq.com>
2019-09-11 17:49:00 +0800

    mapreduce

--- demo/hifunc_mapreduce.py
+++ demo/hifunc_mapreduce.py
@@ -23,4 +23,47 @@ def str2int(str):
     return reduce(fn, map(char2num, str))
 
 
-print(str2int('18954'))
+# print(str2int('18954'))
+
+# 练习
+# 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+# 输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+def standard_name_list(name_list):
+    def standard_name(name):
+        return str.capitalize(name)
+
+    return list(map(standard_name, name_list))
+
+
+# print(standard_name_list(['adam', 'LISA', 'barT']))
+
+
+# 编写一个prod()函数，可以接受一个list并利用reduce()求积：
+def prod(x, y):
+    return x * y
+
+
+# print(reduce(prod,[3, 5, 7, 9]))
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456
+def str2float(s):
+    s_split = str.split(s, '.')
+
+    def char2num(c):
+        return DIGITS[c]
+
+    def fn(x, y):
+        return 10 * x + y
+
+    def fn_dec(x, y):
+        return x / 10 + y
+
+    # 整数部分
+    # print(reduce(fn, map(char2num, s_split[0])))
+    # 小数部分
+    # print(reduce(fn_dec, map(char2num, s_split[1][::-1])))
+    return reduce(fn, map(char2num, s_split[0])) + reduce(fn_dec, map(char2num, s_split[1][::-1])) / 10
+
+
+print(str2float('123.456'))
